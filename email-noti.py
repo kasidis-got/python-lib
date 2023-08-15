248017f5d85d71b9120976cae971a51082b1cff2
kasidis-got <kasidis@got.co.th>
2023-08-15 17:32:41 +0700

    change report name

--- email-noti.py
+++ email-noti.py
@@ -40,7 +40,7 @@ def send_email_with_attachments(subject, body, to_email, file_paths, smtp_detail
 # File paths of the CSVs
 
 file_paths = [
-    "reports"
+    "sastreports"
 ]
 
 smtp_details = {
